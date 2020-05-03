34a68a460af33d51e7f8ec5477a9a6d0a8b763c3
nignanthomas <nignanthomas@gmail.com>
2020-05-03 10:12:23 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# MY_PORTFOLIO
+#### A portfolio and about me website, 08/04/2020
+#### By **nignanthomas**
+## Description
+This website describes who I am, highlights some of my work as a Web Developer, my skills, my background and my contact info if you want to hire me!
+## Live Demo
+https://nignanthomas.github.io
+## Technologies Used
+- HTML
+- CSS
+- JS
+- Bootstrap
+- FormSpree
+## Support and contact details
+Thomas NIGNAN https://github.com/nignanthomas/
+### License
+This project is under MIT License
+Copyright (c) 2019 nignanthomas
---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Disable form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Get the forms we want to add validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+
+// Scroll To Top Button
+scrollButton = document.getElementById("scroll-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollTopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// Change Active Nav Link
+$( '.navbar-nav a' ).on( 'click', function () {
+	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
